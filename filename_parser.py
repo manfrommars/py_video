eb445cf3abe045974171ea2955071a1ea7f41ce8
Elliot S <manfrommars@users.noreply.github.com>
2016-11-16 07:21:55 -0800

    Translate timestamped filenames

--- filename_parser.py
+++ filename_parser.py
@@ -1,27 +1,48 @@
 import datetime
+import os
 
 # Translate filenames encoded with date and timestamps into datetime objects
-
 def datetimeFromFilename(filename):
     date = []
     time = []
+    filename = os.path.splitext(filename)[0]
+    # Translate filenames to canonical YYYYMMDD and HHMMSS form
     if filename.startswith('VID-'):
         # Type 1: VID-YYYYMMDD-WAXXXX
-        # Creation time set to 00:00:00
-        dates = filename.split('-')[1]
-        date = [int(dates[:4]), int(dates[4:6]), int(dates[6:])]
+        date = filename.split('-')[1]
     elif filename.startswith('VID_'):
         # Type 2: VID_YYYYMMDD_HHMMSS
         dates = filename.split('_')
         times = dates[2].strip('.mp4')
-        print(times)
-        dates = dates[1]
-        date = [int(dates[:4]), int(dates[4:6]), int(dates[6:])]
-        time = [int(times[:2]), int(times[2:4]), int(times[4:])]
+        date = dates[1]
+    elif filename[:4].isdigit():
+        # Type 3: YYYY-MM-DD
+        # OR
+        # YYYY-MM-DD HH.MM.SS
+        dates = filename.split(' ')
+        date = dates[0].replace('-', '')
+        if len(dates) > 1:
+            times = dates[1]
+            time = times.replace('.', '')
+    elif filename[:1].isdigit():
+        # Type 4: D[D]-M[M]-YYYY
+        # Must be done after all other date processing
+        if filename.endswith(' (2)'):
+            filename = filename[:-4]
+        dates = filename.split('-')
+        dates = dates[::-1]
+        date = "%04d%02d%02d" % tuple(map(int,dates))
+
+    # Catch unhandled cases, default to 1900-01-01, 00:00:00
     if not time:
-        time = [0, 0, 0]
+        time = "000000"
     if not date:
-        date = [1900, 1, 1]
+        date = "19000101"
+    print(date)
+    # Translate canonical YYYYMMDD and HHMMSS into date and time
+    date = [int(date[:4]), int(date[4:6]), int(date[6:])]
+    time = [int(time[:2]), int(time[2:4]), int(time[4:])]
+          
     print(str(date))
     print(str(time))
     try:
