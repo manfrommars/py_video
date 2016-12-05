90d7c49dc1272c05d28c61d875e14fa189886d16
Elliot S <manfrommars@users.noreply.github.com>
2016-12-04 18:10:54 -0800

    Add basic tests for file input

--- file_item.py
+++ file_item.py
@@ -1,9 +1,14 @@
 # Container for video files
 import os
+import errno
 
 class video_file(object):
     def __init__(self, filepath):
+        # Clean up the filepath
+        filepath = os.path.expanduser(filepath)
         # Verify the file exists
         if not os.path.exists(filepath):
-            raise Exception('Filepath does not exist %s' % filepath)
+            raise FileNotFoundError(errno.ENOENT,
+                                    os.strerror(errno.ENOENT),
+                                    filepath)
         # Gather available data about the file
--- video_file_tester.py
+++ video_file_tester.py
@@ -1,4 +1,26 @@
 import os
 import file_item
+import errno
+import unittest
 
-myfile = file_item.video_file('~/dne')
+class FileInputTesting(unittest.TestCase):
+    def test1(self):
+        self.assertRaises(FileNotFoundError,
+                          file_item.video_file,
+                          '~/dne')
+    def test2(self):
+        try:
+            file_item.video_file('~/Movies/class_video_test/MVI_4299.MOV')
+        except FileNotFoundError:
+            self.fail('file_item.video_file() raised FileNotFoundError ' +
+                      'unexpectedly')
+    def test3(self):
+        try:
+            file_item.video_file('/Users/elliots/Movies/class_video_test/'+
+                                 'MVI_4299.MOV')
+        except FileNotFoundError:
+            self.fail('file_item.video_file() raised FileNotFoundError ' +
+                      'unexpectedly')
+
+if __name__ == '__main__':
+    unittest.main()
