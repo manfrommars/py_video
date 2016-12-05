a93befd14b7829ce5d585b1aa454b2481ea524d5
Elliot S <manfrommars@users.noreply.github.com>
2016-12-04 21:01:10 -0800

    Start adding video items to a scrollable canvas

--- video_tracker.py
+++ video_tracker.py
@@ -1,5 +1,5 @@
 from mp4_parser import mp4_parser
-import filename_parser
+import file_item
 import os
 import sys
 import io
@@ -7,13 +7,16 @@ import tkinter as tk
 
 class Application(tk.Frame):
     def __init__(self, master=None):
+        self.vid_files = []
+        self.filelist = []
         super().__init__(master)
         self.master = master
         self.pack()
         self.create_widgets()
     def create_widgets(self):
         # Search bar
-        self.search_fr = tk.Frame(self.master, borderwidth=1, relief=tk.RAISED)
+        self.search_fr = tk.Frame(self.master, borderwidth=1,
+                                  relief=tk.RAISED)
         self.search_btn = tk.Button(self.search_fr) 
         self.search_btn['text'] = 'Search'
         self.search_btn['command'] = self.search
@@ -24,17 +27,18 @@ class Application(tk.Frame):
         self.e.bind('<Button-1>', self.clear_search)
         self.e.pack(side=tk.LEFT, fill=tk.X, expand=True)
         self.search_fr.pack(side=tk.TOP, fill=tk.X, expand=False)
-        # List box
-        self.filelist = []
-        self.entry_list = tk.Frame(self.master, borderwidth=1, relief=tk.RAISED)
-        self.results_str = tk.StringVar(self.master)
-        self.results_str.set('Hello')
-        self.res = tk.Canvas(self.entry_list, bg='blue')
-##        self.res.create_line(0,0,200,100)
-        self.res.pack(fill=tk.BOTH, expand=True)
-##        self.results = tk.Label(self.entry_list, textvariable=self.results_str)
-##        self.results.pack(fill=tk.BOTH)
+        
+        # Videos listed in a scrollable canvas
+        self.entry_list = tk.Frame(self.master, borderwidth=1,
+                                   relief=tk.RAISED)
+        self.res = tk.Canvas(self.entry_list)
+        self.ybar = tk.Scrollbar(self.entry_list, orient=tk.VERTICAL)
+        self.ybar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.ybar.config(command=self.res.yview)
+        self.res.config(yscrollcommand=self.ybar.set)
+        self.res.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.entry_list.pack(fill=tk.BOTH, expand=True)
+
         # Add Directory button
         # NOTE: Find a way to do this with just one window.
         self.add_dir = tk.Button(self.master, text='Add Directory',
@@ -55,18 +59,31 @@ class Application(tk.Frame):
     def file_selector(self):
         filenames = tk.filedialog.askopenfilename(multiple=True)
         print(filenames)
+        for f in filenames:
+            self.vid_files.append(file_item.video_file(f))
         self.filelist.append(filenames)
-        self.results_str.set(filenames)
+##        self.results_str.set(filenames)
     def dir_selector(self):
         filenames = tk.filedialog.askdirectory()
-        print(filenames)
-        self.filelist.append(filenames)
-        self.results_str.set(filenames)
+        # Walk the directory and add all files
+        for root, dirs, files in os.walk(filenames):
+            for name in files:
+                filepath = os.path.join(root, name)
+                self.vid_files.append(file_item.video_file(filepath))
+                self.filelist.append(filepath)
+##                self.w = tk.Canvas(self.res, height=20)
+##                self.w.create_text(200,10,text=filepath)
+##                self.w.pack(fill=tk.X)
+                self.res.create_text(200, 15 * (len(self.filelist)-1) + 5, text=filepath)
+        print(self.filelist)
+        self.res.config(scrollregion=(0,0,300, len(self.filelist)*15))
+##        self.results_str.set(self.filelist)
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
     root = tk.Tk()
+    root.title('Video Tracker')
     root.geometry("640x480+300+300")
     app = Application(master=root)
     app.mainloop()
